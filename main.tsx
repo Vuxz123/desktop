cab88ca83597dfc897da3af66d894fd8861a1c95
chatgptui <>
2023-03-11 18:46:48 +0900

    Change the icon for text-to-speech
    [skip ci]

--- main.tsx
+++ main.tsx
@@ -284,9 +284,11 @@ const Message = (props: { depth: number }) => {
                             splitLines.end()
                         }
                     }}>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-player-play inline dark:stroke-zinc-300" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-volume inline dark:stroke-zinc-300" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <path d="M7 4v16l13 -8z"></path>
+                        <path d="M15 8a5 5 0 0 1 0 8"></path>
+                        <path d="M17.7 5a9 9 0 0 1 0 14"></path>
+                        <path d="M6 15h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l3.5 -4.5a.8 .8 0 0 1 1.5 .5v14a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5"></path>
                     </svg>
 
                     {/* TODO: stop */}
